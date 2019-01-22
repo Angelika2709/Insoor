70a34454515d6b0343b442254824383a8b959e73
Angelika2709 <46925962+Angelika2709@users.noreply.github.com>
2019-01-22 15:13:30 +0300

    Add files via upload
    Precondition class
    US3 - 80%

---
+++ Precondition.java
@@ -0,0 +1,52 @@
+package us3;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Precondition {
+	public static WebDriver driver;
+	WebElement login, password, signUp, req_but, page, inc_con;
+	public static int linksCount = 0;
+	public static String[] links = null;
+	public String date_values[] = {"{DATE}", "{DATE[][+1M][]}"};
+
+	protected void setup_enviroment() {
+		System.setProperty("webdriver.chrome.driver", "C://chromedriver_win32//chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://sotsos:St.Petersburg@insoor.com");
+	}
+
+	protected void autoriz() throws InterruptedException {
+		login = driver.findElement(By.id("loginform-email"));
+		password = driver.findElement(By.id("loginform-password"));
+		login.sendKeys("Anzhela.Barkovskaia@sotsos.com");
+		password.sendKeys("tosca!123");
+		signUp = driver.findElement(By.name("login-button"));
+		signUp.submit();
+		// ожидание загрузки кнопки
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[2]/a/span")));
+	}
+
+	// список меню
+	protected void main_sidebar() throws Exception {
+		List<WebElement> linksize = driver.findElements(By.tagName("a"));
+		linksCount = linksize.size();
+		links = new String[linksCount];
+		for (int i = 0; i < linksCount; i++) {
+			links[i] = linksize.get(i).getAttribute("href");
+		}
+	}
+
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
---
+++ Request_new_offer.java
@@ -0,0 +1,110 @@
+package us3;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Request_new_offer extends Precondition {
+	public static String[] statuses, types, per;
+	String dates = null;
+	WebElement req_but, field_mar_st, field_ins_types, field_pay_per, male, female, transgender, beg_per, end_per,
+			submit;
+
+	public void req_new_Straight_Through() throws Exception {
+
+		female.click();
+		field_mar_st.sendKeys(statuses[3]);
+		field_ins_types.sendKeys(types[2]);
+		beg_per.sendKeys("date_values[0]");
+		end_per.sendKeys("date_values[1]");
+		field_pay_per.sendKeys(per[2]);
+	}
+
+	public void req_new() throws Exception {
+		req_but = driver.findElement(By.id("new_offer"));
+		req_but.click();
+
+		// ожидание загрузки кнопки
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("modalHeader")));
+
+		get_gender();
+		get_mar_st();
+		get_pay_per();
+		get_ins_types();
+		get_data();
+		Thread.sleep(5000);
+		submit();
+	}
+
+	protected void get_data() {
+		beg_per = driver.findElement(By.id("start_datepicker"));
+		end_per = driver.findElement(By.id("end_datepicker"));
+		for (int n = 0; n < date_values.length; n++) {
+			dates = dates + date_values[n];
+		}		
+	}
+
+	protected void submit() {
+		submit = driver.findElement(By.xpath("/html/body/div[1]/div/section[2]/section/div/div[1]/div/div[3]/div/div/div[2]/div/form/div[2]/button[2]"));
+		submit.click();
+	}
+
+	protected void get_mar_st() {
+		Select dropdown_mar_st = new Select(driver.findElement(By.xpath("//*[@id=\"customer-maritalstatus\"]")));
+		List<WebElement> dd_mar_st = dropdown_mar_st.getOptions();
+		statuses = new String[linksCount];
+		for (int j = 0; j < dd_mar_st.size(); j++) {
+			statuses[j] = dd_mar_st.get(j).getText();
+		}
+		field_mar_st = driver.findElement(By.id("customer-maritalstatus"));
+	}
+
+	protected void get_ins_types() {
+		Select dropdown_ins_types = new Select(driver.findElement(
+				By.xpath("//*/form/div/div[4]/div/select[@id=\"userinsurancetypecompany-insurance_type_id\"]")));
+		List<WebElement> dd_ins_types = dropdown_ins_types.getOptions();
+		types = new String[linksCount];
+		for (int j = 0; j < dd_ins_types.size(); j++) {
+			types[j] = dd_ins_types.get(j).getText();
+		}
+		field_ins_types = driver.findElement(
+				By.xpath("//*/form/div/div[4]/div/select[@id=\"userinsurancetypecompany-insurance_type_id\"]"));
+	}
+
+	protected void get_pay_per() {
+		Select dropdown_pay_per = new Select(
+				driver.findElement(By.xpath("//*[@id=\"userinsurancetypecompany-payment_period_type\"]")));
+		List<WebElement> dd_pay_per = dropdown_pay_per.getOptions();
+		per = new String[linksCount];
+		for (int l = 0; l < dd_pay_per.size(); l++) {
+			per[l] = dd_pay_per.get(l).getText();
+		}
+		field_pay_per = driver.findElement(By.id("userinsurancetypecompany-payment_period_type"));
+	}
+
+	protected void get_gender() {
+		male = driver.findElement(By.xpath("//label[@for='Customer[gender]0']"));
+		female = driver.findElement(By.xpath("//label[@for='Customer[gender]1']"));
+		transgender = driver.findElement(By.xpath("//label[@for='Customer[gender]2']"));
+	}
+
+	public static void main(String[] args) throws Exception {
+		Request_new_offer req = new Request_new_offer();
+		req.setup_enviroment();
+		req.autoriz();
+		req.main_sidebar();
+		driver.navigate().to(links[28]);
+		req.req_new();
+		req.req_new_Straight_Through();
+
+	}
+
+}
