2e0066d855d620df26298987fc11ea23ac529b33
USER 1 <Anzhela.Barkovskaia@sotsos.com>
2019-02-05 11:56:09 +0300

    ispravleno

--- Insoor/src/us1/Random.java
+++ Insoor/src/us1/Random.java
@@ -1,25 +1,25 @@
 package us1;
 
-public class Random extends Digital_sign{
+public class Random {
 	
 	
-	public int rand_int1 (int x) {
+	public int get_int1 (int x) {
 		return x = 100 + (int)(Math.random()*(999));	
 	}
 	
-	public int rand_int2 (int x) {
+	public int get_int2 (int x) {
 		return x = 10 + (int)(Math.random()*(99));	
 	}
 	
-	public int rand_int3 (int x) {
+	public int get_int3 (int x) {
 		return x = 100000001 + (int)(Math.random()*(999999999));	
 	}
 	
-	public int rand_int4 (int x) {
+	public int get_int4 (int x) {
 		return x = 999 + (int)(Math.random()*(9999));		
 	}
 	
-	public int rand_int5 (int x) {
+	public int get_int5 (int x) {
 		return x = 100001 + (int)(Math.random()*(999999));		
 	}
 
--- Insoor/src/us1/StraightThrough.java
+++ Insoor/src/us1/StraightThrough.java
@@ -8,49 +8,55 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 
-class StraightThrough extends Random{
+class StraightThrough extends Random {
+	
+	private Random rand = new Random();
+	private Precondition pr;
+	private Sign_up sign_up = new Sign_up();
+	private Sign_up_verification sign_up_ver = new Sign_up_verification();;
+	private Personal_data per_data = new Personal_data();
+	private Digital_sign dig_sign = new Digital_sign();
+	
 
 	@BeforeEach
-	public void setUp() {
-		setUp();		
+	public void setUp() {	
+		pr = new Precondition();		
+		pr.setUp();
+	}		
+
+	@Test
+	void test() {
+		sign_up.getSignUp();
+		sign_up.email.sendKeys("TDM_EMAIL@" + rand.get_int1(1) + ".com");
+		sign_up.password.sendKeys("tosca!" + rand.get_int1(1));
+		sign_up.phonecode.sendKeys(Integer.toString(rand.get_int2(1)));
+		sign_up.phone.sendKeys(Integer.toString(rand.get_int3(1)));
+		sign_up.submit.sendKeys(Keys.ENTER);
+
+		sign_up_ver.getSignUpVerif();
+		sign_up_ver.code.sendKeys(Integer.toString(rand.get_int4(1)));
+		sign_up_ver.ver_but.submit();
+
+		per_data.getPersonalData();
+		per_data.first_name.sendKeys("John_" + rand.get_int1(1));
+		per_data.last_name.sendKeys("Smith_" + rand.get_int1(1));
+		per_data.address.sendKeys("Mira street 3/438");
+		per_data.post_code.sendKeys(Integer.toString(rand.get_int5(1)));
+		per_data.city.sendKeys("Saint-Petersbyrg");
+		per_data.birthday.sendKeys("27/09/1988");
+		per_data.conf_but.submit();
+
+		dig_sign.getDig_Sign();
+		Actions a = new Actions(Precondition.driver);
+		a.build();
+		a.moveToElement(dig_sign.canvas, 10, 5).clickAndHold(dig_sign.canvas).moveByOffset(5, 5).release(dig_sign.canvas);
+		a.perform();
+		dig_sign.fin_but.submit();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-		driver.quit();
+		Precondition.driver.quit();
 	}
-	
-
-	@Test
-	void test(int x) {
-		getSignUp();		
-		email.sendKeys("TDM_EMAIL@"+rand_int1(x)+".com");
-		password.sendKeys("tosca!"+rand_int1(x));
-		phonecode.sendKeys(Integer.toString(rand_int2(x)));
-		phone.sendKeys(Integer.toString(rand_int3(x)));
-		submit.sendKeys(Keys.ENTER);
-		
-		getSignUpVerif();
-		code.sendKeys(Integer.toString(rand_int4(x)));
-		ver_but.submit();
-	
-		getPersonalData();
-		first_name.sendKeys("John_"+rand_int1(x));
-		last_name.sendKeys("Smith_"+rand_int1(x));
-		address.sendKeys("Mira street 3/438");
-		post_code.sendKeys(Integer.toString(rand_int5(x)));
-		city.sendKeys("Saint-Petersbyrg");
-		birthday.sendKeys("27/09/1988");
-		conf_but.submit();		
-		
-		getDig_Sign();
-		Actions a = new Actions(driver);
-		a.build();
-		a.moveToElement(canvas,10,5).clickAndHold(canvas).moveByOffset(5,5).release(canvas); 
-		a.perform();
-		fin_but.submit();
-	}
-
-
 
 }
