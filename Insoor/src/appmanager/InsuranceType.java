67e963c3076a93aff24e3f425e47d82a74040aaa
USER 1 <Anzhela.Barkovskaia@sotsos.com>
2019-02-13 11:52:10 +0300

    poka ne rabotaet

--- Insoor/src/appmanager/InsuranceType.java
+++ Insoor/src/appmanager/InsuranceType.java
@@ -3,17 +3,20 @@ package appmanager;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import appmanager.*;
 import model.InsTypes;
 
 public class InsuranceType extends HelperBase {
 	private NavigationHelper navigationHelper;
-	
-
+	WebDriverWait wait = new WebDriverWait(wd, 10);
+	public WebElement error = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@id='form-policy-add']/div/div[2]/div/p")));
 
 	public InsuranceType(WebDriver wd) {
 		super(wd);
@@ -23,7 +26,6 @@ public class InsuranceType extends HelperBase {
 	public void getTypes(InsTypes insttypes) {
 		sendValues(By.xpath("//*/form/div/div[2]/div/select[@id=\"userinsurancetypecompany-insurance_type_id\"]"),
 				insttypes.returnInstTypes());
-		boolean err_type = wd.findElement());
 		
 	}
 }
--- Insoor/src/tests/UploadNewPolicy.java
+++ Insoor/src/tests/UploadNewPolicy.java
@@ -15,30 +15,29 @@ import appmanager.InsuranceType;
 import appmanager.NavigationHelper;
 
 class UploadNewPolicy extends TestBase {
-
 	
+
 	@Test
 	void testRequestOffer() throws Exception {
 		app.getNavigationHelper().main_sidebar();
 		Thread.sleep(5000);
 		app.getNavigationHelper().go_to_policy();
-		Thread.sleep(3000);	
-		
-		app.getInsuranceType().getTypes(new InsTypes("All types"));
-		
-		Assert.assertTrue(wd.findElement(By.xpath("//form[@id='form-policy-add']/div/div[2]/div/p")).isDisplayed());
-		
+		Thread.sleep(3000);
+
+		app.getInsuranceType().getTypes(new InsTypes("All types"));	
 		app.getInsurerName().getInsurer(new InsNames("PAX"));
 		app.datesHelper.data("12/02/2019", "12/02/2020");
-		Thread.sleep(3000);	
+		Thread.sleep(3000);
 		app.priceHelper.price("200");
-		Thread.sleep(3000);	
+		Thread.sleep(3000);
+		boolean dd = app.getInsuranceType().getTypes();
+		if (dd == true){
+			app.getInsuranceType().error.getText();
+			System.out.println("Insurance Type ID cannot be blank.");
+		}
+		else {
 		app.getNavigationHelper().submit_pol();
 		app.getNavigationHelper().close_pol();
-		app.getNavigationHelper().sign_out();
-		
-		
 	}
-
 }
-
+}
